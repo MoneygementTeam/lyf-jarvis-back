fc9d90d82119d270d8905fc7d1734d8a1d8770f6
bk100km <bk100km@naver.com>
2024-10-26 17:22:05 +0900

    :sparkles: [AJ-7] User 로그인, 공통 기능 추가

---
+++ src/main/java/com/moneygement/lyf/jarvis/common/config/WebConfig.java
@@ -0,0 +1,32 @@
+package com.moneygement.lyf.jarvis.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOriginPatterns("*", "http://localhost:8080", "http://localhost:5173", "/**",
+				"https://localhost:5173", "http://127.0.0.1:5173")
+			.allowedMethods("GET", "POST", "PUT", "DELETE")
+			.allowedHeaders("*")
+			.allowCredentials(true);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/docs/**")
+			.addResourceLocations("classpath:/static/docs/");
+		registry.addResourceHandler("/**")
+			.addResourceLocations("classpath:/templates/");
+		registry.addResourceHandler("/favicon.ico")
+			.addResourceLocations("classpath:/static/");
+		registry.addResourceHandler("/**")
+			.addResourceLocations("classpath:/static/");
+	}
+
+}
