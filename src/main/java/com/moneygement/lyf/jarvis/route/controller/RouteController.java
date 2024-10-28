1c6016c97ea2f052f36fe59b02ce8a30a74a9a53
Tina <migujeong@gmail.com>
2024-10-28 23:25:41 +0900

    ♻️ 최단 경로 API에서 불필요한 응답값 제거

--- src/main/java/com/moneygement/lyf/jarvis/route/controller/RouteController.java
+++ src/main/java/com/moneygement/lyf/jarvis/route/controller/RouteController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.moneygement.lyf.jarvis.route.domain.OptimalRouteRequest;
 import com.moneygement.lyf.jarvis.route.domain.OptimalRouteResponse;
+import com.moneygement.lyf.jarvis.route.domain.OptimalRouteUserResponse;
 import com.moneygement.lyf.jarvis.route.service.RouteService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -29,9 +30,9 @@ public class RouteController {
 
 	@PostMapping("")
 	@Operation(summary = "최단 경로 정보 제공", description = "좌표와 교통수단 기준의 최단 경로 정보를 제공합니다.")
-	public ResponseEntity<OptimalRouteResponse> getOptimalRouteRequest(@RequestBody OptimalRouteRequest optimalRouteRequest)
+	public ResponseEntity<OptimalRouteUserResponse> getOptimalRouteRequest(@RequestBody OptimalRouteRequest optimalRouteRequest)
 		throws IOException {
 		OptimalRouteResponse optimalRoute = routeService.getOptimalRoute(optimalRouteRequest);
-		return ResponseEntity.ok(optimalRoute);
+		return ResponseEntity.ok(new OptimalRouteUserResponse(optimalRoute.getPaths().get(0).getPoints().getCoordinates()));
 	}
 }
---
+++ src/main/java/com/moneygement/lyf/jarvis/route/domain/OptimalRouteUserResponse.java
@@ -0,0 +1,6 @@
+package com.moneygement.lyf.jarvis.route.domain;
+
+import java.util.List;
+
+public record OptimalRouteUserResponse(List<List<Double>> coordinates) {
+}
